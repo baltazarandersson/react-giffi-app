9892b3413838784ce861bee8177b97570fa82412
Baltazar <balti.kpo@gmail.com>
2022-03-10 20:52:16 -0300

    gif search done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import ListOfGifs from "./components/ListOfGifs";
+import SearchGifs from "./components/SearchGifs";
 
 function App() {
+  const [keyword, setKeyword] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <SearchGifs setKeyword={setKeyword} />
+        <ListOfGifs keyword={keyword} />
+      </section>
     </div>
   );
 }
