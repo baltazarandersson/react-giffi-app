90cbc4274c40bb1c055722c0181821bcc9f726db
Baltazar <balti.kpo@gmail.com>
2022-03-15 11:56:04 -0300

    loader to details

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <>
+      <div>
+        <div className="fulfilling-square-spinner">
+          <div className="spinner-inner"></div>
+        </div>
+      </div>
+    </>
+  );
+}
