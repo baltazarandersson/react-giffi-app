7157c9cd5e68cba3fd3b114601a10d568aba8860
Baltazar <balti.kpo@gmail.com>
2022-03-15 14:23:11 -0300

    hook files added

---
+++ src/components/HomeGifs.js
@@ -0,0 +1,33 @@
+import { React } from "react";
+import useGifs from "../hooks/useGifs";
+import Gif from "./Gif";
+import Loading from "./Loading";
+import "./HomeGifs.css";
+
+export default function HomeGifs() {
+  const { gifsState } = useGifs({ keyword: "coding" });
+
+  if (gifsState.loading) return <Loading />;
+
+  return (
+    <>
+      <div className="home-nav">
+        GIF search engine app made with
+        <strong>&nbsp;React&nbsp;</strong>
+        powered by
+        <strong>&nbsp;GIPHY API</strong>
+      </div>
+      <div className="gifs-container">
+        {gifsState.gifs.map(({ title, import_datetime, url, id }) => (
+          <Gif
+            key={id}
+            title={title}
+            url={url}
+            date={import_datetime}
+            id={id}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ src/hooks/useGifs.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import getGifs from "../services/getGifs";
+import "../components/Loading.css";
+import "../components/ListOfGifs.css";
+
+export default function useGifs({ keyword }) {
+  const [gifsState, setGifsState] = useState({ loading: false, gifs: [] });
+
+  useEffect(() => {
+    setGifsState((prevState) => {
+      return { ...prevState, loading: true };
+    });
+    getGifs({ keyword }).then((gifs) => {
+      setGifsState({ loading: false, gifs: gifs });
+    });
+  }, [keyword]);
+
+  return { gifsState };
+}
