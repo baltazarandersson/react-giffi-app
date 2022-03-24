fb66aafd68bc31ccf928c9cf9bbca49a5ece237c
Baltazar <balti.kpo@gmail.com>
2022-03-24 19:33:55 -0300

    Added Infinite scroll

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,21 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useCallback } from "react";
 import Loading from "./Loading";
 import useGifs from "../hooks/useGifs";
 import GifsContainer from "./GifsContainer";
 import "./ListOfGifs.css";
+import useNearScreen from "hooks/useNearScreen";
+import debounce from "just-debounce-it";
 
 export default function ListOfGifs({ params }) {
   const { keyword } = params;
   const { gifsState, setPage } = useGifs({ keyword });
+  const externalRef = useRef();
+  const { isNearScreen } = useNearScreen({
+    externalRef: gifsState.loading ? null : externalRef,
+    once: false,
+  });
 
-  const handleNextPage = () => setPage((prevPage) => prevPage + 1);
+  const debounceHandleNextPage = useCallback(
+    debounce(() => setPage((prevPage) => prevPage + 1), 200),
+    []
+  );
+
+  useEffect(() => {
+    if (isNearScreen) debounceHandleNextPage();
+  }, [isNearScreen, debounceHandleNextPage]);
 
   if (gifsState.loading) return <Loading />;
 
   return (
     <div className="list-of-gifs">
       <GifsContainer gifsState={gifsState} />
-      <button onClick={handleNextPage}>Load More GIFs</button>
+      <div className="visor" ref={externalRef}></div>
     </div>
   );
 }
--- src/hooks/useNearScreen.js
+++ src/hooks/useNearScreen.js
@@ -1,25 +1,39 @@
 import { useEffect, useRef, useState } from "react";
 
-export default function useNearScreen({ distance = "100px" } = {}) {
-  const fromRef = useRef();
+export default function useNearScreen({
+  distance = "100px",
+  externalRef,
+  once = true,
+} = {}) {
   const [isNearScreen, setShow] = useState(false);
+  const fromRef = useRef();
 
   useEffect(function () {
+    let observer;
+    const element = externalRef ? externalRef.current : fromRef.current;
+
     const onChange = (entries, observer) => {
       const el = entries[0];
       if (el.isIntersecting) {
         setShow(true);
-        observer.disconnect();
+        once && observer.disconnect();
+      } else {
+        !once && setShow(false);
       }
     };
 
-    const observer = new IntersectionObserver(onChange, {
-      rootMargin: distance,
+    Promise.resolve(
+      typeof IntersectionObserver !== "undefined"
+        ? IntersectionObserver
+        : import("intersection-observer")
+    ).then(() => {
+      observer = new IntersectionObserver(onChange, {
+        rootMargin: distance,
+      });
+      if (element) observer.observe(element);
     });
 
-    observer.observe(fromRef.current);
-
-    return () => observer.disconnect();
+    return () => observer && observer.disconnect();
   });
 
   return { isNearScreen, fromRef };
