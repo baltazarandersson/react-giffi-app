407664cfebc3fcd977f83aa10e1fb41f906e7a1d
Baltazar <balti.kpo@gmail.com>
2022-03-25 13:11:35 -0300

    Deleted Trending Box on search

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import React from "react";
 import App from "./App";
 
 test("renders without crashing", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/GIFFI/i);
+  expect(title).toBeInTheDocument();
 });
--- src/pages/SearchResult/SearchResult.js
+++ src/pages/SearchResult/SearchResult.js
@@ -1,11 +1,9 @@
 import ListOfGifs from "components/ListOfGifs";
-import TrendingBox from "components/TrendingBox";
 
 export default function SearchResult({ params }) {
   return (
     <div className="main">
       <ListOfGifs params={params}></ListOfGifs>
-      <TrendingBox></TrendingBox>
     </div>
   );
 }
