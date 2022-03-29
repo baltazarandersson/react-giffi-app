d32eab98e30033df5eb4560a4cad95e00d21d92b
Baltazar <balti.kpo@gmail.com>
2022-03-28 22:29:25 -0300

    Fullscreen button on gif detail

--- src/components/GifDetail.js
+++ src/components/GifDetail.js
@@ -1,11 +1,16 @@
+import React, { useRef, useState } from "react";
 import useSingleGif from "hooks/useSingleGif";
 import "./GifDetail.css";
 import Loading from "./Loading";
 import { Redirect } from "wouter";
+import { AiOutlineFullscreen, AiOutlineFullscreenExit } from "react-icons/ai";
+import classNames from "classnames";
 
 export default function GifDetail({ params }) {
   const { id } = params;
   const { gif, isLoading, isError } = useSingleGif(id);
+  const videoRef = useRef();
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
   if (isLoading) return <Loading />;
   if (isError) return <Redirect to="/404" />;
@@ -13,10 +18,34 @@ export default function GifDetail({ params }) {
 
   const { title, date, bitly_url, mp4_url } = gif;
 
+  function toggleFullscreen() {
+    setIsFullscreen(!isFullscreen);
+
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      videoRef.current.requestFullscreen();
+    }
+  }
+
   return (
-    <div className="gif-detail">
-      <video controls loop autoPlay="autoplay" src={mp4_url} alt="gif-movie" />
-      <div>
+    <div className="gif-detail-container">
+      <div className="gif-detail-player" ref={videoRef}>
+        <video muted loop autoPlay="autoplay" src={mp4_url} alt="gif-movie" />
+        <button onClick={() => toggleFullscreen()}>
+          <AiOutlineFullscreen
+            className={classNames("fullscreen-icon", {
+              "fullscreen-icon-visible": !isFullscreen,
+            })}
+          />
+          <AiOutlineFullscreenExit
+            className={classNames("fullscreen-icon", {
+              "fullscreen-icon-visible": isFullscreen,
+            })}
+          />
+        </button>
+      </div>
+      <div className="gif-detail-info">
         <h1>{title}</h1>
         <p>{date}</p>
         <a href={bitly_url}>source</a>
--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -6,23 +6,27 @@ import { useGifsContext } from "context/GifsContext";
 let INITIAL_PAGE = 0;
 
 export default function useGifs({ keyword }) {
-  // const [gifsState, setGifsState] = useState({ loading: false, gifs: [] });
-
   const [page, setPage] = useState(INITIAL_PAGE);
   const [loading, setLoading] = useState(false);
   const { gifs, setGifs } = useGifsContext();
+  const lastKeyword = localStorage.getItem("lastKeyword");
 
   useEffect(() => {
-    setLoading(true);
-    getGifs({ keyword }).then((gifs) => {
-      setLoading(false);
-      setGifs(gifs);
-      localStorage.setItem("lastKeyword", keyword);
-    });
-  }, [keyword, setGifs]);
+    if (lastKeyword === keyword) {
+      return gifs;
+    } else {
+      setLoading(true);
+      getGifs({ keyword }).then((gifs) => {
+        setLoading(false);
+        setGifs(gifs);
+        localStorage.setItem("lastKeyword", keyword);
+      });
+    }
+  }, [keyword, setGifs, gifs, lastKeyword]);
 
   useEffect(() => {
     if (page === INITIAL_PAGE) return;
+    console.log(page);
     getGifs({ keyword, page }).then((nextGifs) => {
       setGifs((prevState) => prevState.concat(nextGifs));
     });
