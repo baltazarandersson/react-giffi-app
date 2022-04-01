787bcc10a0fcb56dca91831b074c659b7d649c59
Baltazar <balti.kpo@gmail.com>
2022-04-01 15:04:11 -0300

    Improved trending box design

--- src/services/getRandomColor.js
+++ src/services/getRandomColor.js
@@ -1,6 +1,9 @@
 export default function getRandomColor() {
   const randomNumber = (max, min) =>
     Math.floor(Math.random() * (max - min + 1) + min);
-  const color = `hsla(${randomNumber(0, 360)}, 90%, 70%, 1)`;
+  const color = `hsla(${randomNumber(0, 360)}, ${randomNumber(
+    70,
+    100
+  )}%, ${randomNumber(65, 75)}%, 1)`;
   return color;
 }
