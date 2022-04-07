dde2a89a35afded9ba29b1965b7203805b9becc0
Baltazar <balti.kpo@gmail.com>
2022-04-07 17:33:52 -0300

    Header hidden on scroll and fixed modal height bug on mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,10 +4,29 @@ import ThemeSwitch from "components/ThemeSwitch";
 import Drawer from "components/Drawer";
 import "./index.css";
 import { UserButtons } from "components/UserButtons";
+import classNames from "classnames";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [prevPosition, setPrevPosition] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      let moving = window.pageYOffset;
+
+      console.log("prevPosition", prevPosition, "moving", moving);
+
+      setVisible(prevPosition > 100 ? prevPosition > moving : true);
+      setPrevPosition(moving);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
-    <div className="header">
+    <div className={classNames("header", { "header--hidden": !visible })}>
       <div className="header__div header__div--1">
         <Link className="header__logo" to="/">
           GIFFI
